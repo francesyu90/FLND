2e194dac89da6e697d8312c0db8710e799553b17
Frances Yu <francesyu90@yahoo.com>
2017-09-30 14:23:18 -0700

    Get graph working for T120 & T361

--- test.py
+++ test.py
@@ -2,11 +2,34 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-excelData = pd.read_excel(open('RealValue.xlsx','rb'), sheetname='Sheet2', skiprows = range(1, 2))
+if __name__ == '__main__':
 
-timeData = pd.to_datetime(excelData["time"], errors='coerce')
-t120Data = pd.to_numeric(excelData["value"], errors='coerce')
+	excelDataT120 = pd.read_excel(
+		open('RealValue.xlsx','rb'), 
+		sheetname='Sheet2', 
+		skiprows = range(1, 2))
+	excelDataT361 = pd.read_excel(
+		open('RealValueT361.xlsx','rb'), 
+		sheetname='Sheet1', 
+		skiprows = range(1, 2))
 
-plt.plot(timeData, t120Data)
-plt.savefig("testT120.png")
-plt.show()
+	timeT120Data = pd.to_datetime(excelDataT120["time"], errors='coerce')
+	t120Data = pd.to_numeric(excelDataT120["value"], errors='coerce')
+	timeT361Data = pd.to_datetime(excelDataT361["time"], errors='coerce')
+	t361Data = pd.to_numeric(excelDataT361["value"], errors='coerce')
+
+	fig, ax1 = plt.subplots()
+	ax2 = ax1.twinx()
+
+	ax1.plot(timeT120Data, t120Data, 'k-', label='Cell01')
+	ax1.set_xlabel('Timestamp')
+	ax1.set_ylabel('RowA.VisioFroth.RGBColor')
+	ax2.plot(timeT361Data, t361Data, 'b-', label='Cell03')
+	
+
+	# plt.plot(timeData, t120Data)
+	fig.tight_layout()
+	plt.legend(loc='upper left')
+	plt.savefig("testT120A361.png")
+	plt.show()
+
