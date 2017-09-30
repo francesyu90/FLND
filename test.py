01bb69b51e248fd99eb8399fc2743682566b0317
Frances Yu <francesyu90@yahoo.com>
2017-09-30 13:20:14 -0700

    Get plot working for tag 120

--- test.py
+++ test.py
@@ -2,12 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-excelData = pd.read_excel(open('Flows.xlsx','rb'), sheetname='Flows')
+excelData = pd.read_excel(open('RealValue.xlsx','rb'), sheetname='Sheet2', skiprows = range(1, 2))
 
-timeData = pd.to_datetime(excelData["Timestamp"])
-znData = pd.to_numeric(excelData["Zn Rougher 2 Con Flow"], errors='coerce')
-# print(znData)
+timeData = pd.to_datetime(excelData["time"], errors='coerce')
+t120Data = pd.to_numeric(excelData["value"], errors='coerce')
 
-plt.plot(timeData, znData)
-plt.savefig("test.png")
+plt.plot(timeData, t120Data)
+plt.savefig("testT120.png")
 plt.show()
