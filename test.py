006393ca8a2707399879a615264896e22f0246f3
Frances Yu <francesyu90@yahoo.com>
2017-09-30 19:25:15 -0700

    Used T117 as x-axis & T120 as y-axis

--- test.py
+++ test.py
@@ -15,25 +15,16 @@ def main():
 		skiprows = range(1, 3),
 		parse_dates=['time'])
 
-	timeT117Data = pd.to_datetime(excelDataT117["time"], errors='coerce')
 	rvT117Data = pd.to_numeric(excelDataT117["value"], errors='coerce')
-	timeT120Data = pd.to_datetime(excelDataT120["time"], errors='coerce')
 	rvT120Data = pd.to_numeric(excelDataT120["value"], errors='coerce')
-
-	plt.figure()
-	plt.subplot(211)
-	lineT117, = plt.plot(timeT117Data, rvT117Data, color='b', label="T117 RowA.Cell01.VisioFroth.Cell01.LAB_Luminance")
-
-	plt.subplot(212)
-	lineT120, = plt.plot(timeT120Data, rvT120Data, color='k', label="T120 RowA.Cell01.VisioFroth.Cell01.RGBColor")
 	
-	plt.subplot(211)
-	# plt.legend(handles=[lineT120, lineT361, lineT595], loc = 0)
+	plt.plot(rvT117Data, rvT120Data, 'r^')
 
-	# Put a legend below current axis
-	plt.legend(loc='upper right', handles=[lineT117, lineT120])
-
-	plt.savefig("T117VsT120.png")
+	plt.xlabel('T117 RowA.Cell01.VisioFroth.Cell01.LAB_Luminance')
+	plt.ylabel('T120 RowA.Cell01.VisioFroth.Cell01.RGBColor')
+	# plt.title('')
+	plt.grid(True)
+	plt.savefig("T117VsT120I.png")
 	plt.show()
 
 if __name__ == '__main__': 
