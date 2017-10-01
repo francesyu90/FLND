75dc01834cff6ac7328461cbfca3e4401ab39809
Frances Yu <francesyu90@yahoo.com>
2017-09-30 21:55:45 -0700

    Plotted T592 Vs T536

--- test.py
+++ test.py
@@ -9,22 +9,22 @@ def main():
 		sheetname='Sheet1', 
 		skiprows = range(1, 3),
 		parse_dates=['time'])
-	excelDataT535 = pd.read_excel(
-		open('FilteredRealValueT535.xlsx','rb'), 
+	excelDataT536 = pd.read_excel(
+		open('FilteredRealValueT536.xlsx','rb'), 
 		sheetname='Sheet1', 
 		skiprows = range(1, 3),
 		parse_dates=['time'])
 
 	rvT592Data = pd.to_numeric(excelDataT592["value"], errors='coerce')
-	rvT535Data = pd.to_numeric(excelDataT535["value"], errors='coerce')
+	rvT536Data = pd.to_numeric(excelDataT536["value"], errors='coerce')
 
-	plt.plot(rvT592Data, rvT535Data, 'r^')
+	plt.plot(rvT592Data, rvT536Data, 'r^')
 
 	plt.xlabel('T592 RowA.Cell05.VisioFroth.Cell05.LAB_Luminance')
-	plt.ylabel('T535 RowA.Cell05.VisioFroth.Cell05.XVelocity')
-	plt.title('T592 Vs T535')
+	plt.ylabel('T536 RowA.Cell05.VisioFroth.Cell05.YVelocity')
+	plt.title('T592 Vs T536')
 	plt.grid(True)
-	plt.savefig("T592VsT535.png")
+	plt.savefig("T592VsT536.png")
 	plt.show()
 
 if __name__ == '__main__': 
