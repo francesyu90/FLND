8d9480a506997e813814eed516c2d46e98bca328
Frances Yu <francesyu90@yahoo.com>
2017-09-30 19:54:48 -0700

    Plotted T61 VS Timestamp

--- test.py
+++ test.py
@@ -20,17 +20,18 @@ def main():
 		skiprows = range(1, 3),
 		parse_dates=['time'])
 
+	timeData = pd.to_datetime(excelDataT117["time"], errors='coerce')
 	rvT117Data = pd.to_numeric(excelDataT117["value"], errors='coerce')
 	rvT61Data = pd.to_numeric(excelDataT61["value"], errors='coerce')
 	rvT60Data = pd.to_numeric(excelDataT60["value"], errors='coerce')
 	
-	plt.plot(rvT60Data, rvT61Data, 'r^')
+	plt.plot(timeData, rvT61Data)
 
-	plt.xlabel('T60 RowA.Cell01.VisioFroth.Cell01.XVelocity')
+	plt.xlabel('Timestamp')
 	plt.ylabel('T61 RowA.Cell01.VisioFroth.Cell01.YVelocity')
 	# plt.title('')
 	plt.grid(True)
-	plt.savefig("T60VsT61I.png")
+	plt.savefig("timeVsT61I.png")
 	plt.show()
 
 if __name__ == '__main__': 
