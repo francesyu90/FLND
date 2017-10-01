164df698f0d2148ba6021d8c4c94bedd6009fc44
Frances Yu <francesyu90@yahoo.com>
2017-09-30 20:06:24 -0700

    Plotted T60, T61, T117 Vs Timestamp

--- test.py
+++ test.py
@@ -24,14 +24,24 @@ def main():
 	rvT117Data = pd.to_numeric(excelDataT117["value"], errors='coerce')
 	rvT61Data = pd.to_numeric(excelDataT61["value"], errors='coerce')
 	rvT60Data = pd.to_numeric(excelDataT60["value"], errors='coerce')
-	
-	plt.plot(timeData, rvT61Data)
+
+	plt.figure()
+	plt.subplot(311)
+	lineT60, = plt.plot(timeData, rvT60Data, color='b', label="T60")
+
+	plt.subplot(312)
+	lineT61, = plt.plot(timeData, rvT61Data, color='k', label="T61")
+
+	plt.subplot(313)
+	lineT117, = plt.plot(timeData, rvT117Data, color='r', label="T117")
 
 	plt.xlabel('Timestamp')
-	plt.ylabel('T61 RowA.Cell01.VisioFroth.Cell01.YVelocity')
+	# plt.ylabel('T61 RowA.Cell01.VisioFroth.Cell01.YVelocity')
 	# plt.title('')
-	plt.grid(True)
-	plt.savefig("timeVsT61I.png")
+	# plt.grid(True)
+	plt.subplot(311)
+	plt.legend(loc='upper right', handles=[lineT60, lineT61, lineT117])
+	plt.savefig("timeVsT60AT61T117.png")
 	plt.show()
 
 if __name__ == '__main__': 
